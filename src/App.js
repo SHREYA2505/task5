27e2a34c16f9dfb43d511e66bfa1c5a1313eca5d
ronithvalluri <ronithvalluri11@gmail.com>
2023-10-13 22:10:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AgeCalculator from './components/AgeCalculator.js';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
